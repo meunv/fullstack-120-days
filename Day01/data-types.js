9c2e3e9eada60ac0414e0cbed5465363fe43e0b0
meunv <meunv234@gmail.com>
2026-07-18 18:08:21 +0530

    Day 1 completed

---
+++ Day01/data-types.js
@@ -0,0 +1,18 @@
+let name = "John Doe";
+
+let age = 25;
+
+let salary = 50000.5;
+
+let isStudent = false;
+
+let country = null;
+
+let email;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof salary);
+console.log(typeof isStudent);
+console.log(typeof country);
+console.log(typeof email);
---
+++ Day01/operators.js
@@ -0,0 +1,12 @@
+let a = 20;
+let b = 10;
+
+console.log(a + b);
+
+console.log(a - b);
+
+console.log(a * b);
+
+console.log(a / b);
+
+console.log(a % b);
